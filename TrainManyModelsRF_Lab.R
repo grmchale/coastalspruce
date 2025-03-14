c2091ca7b5cd908e818b60545747e4c7cdedf44c
grmchale <152335438+grmchale@users.noreply.github.com>
2025-03-14 17:17:13 -0400

    Fidgeting with RF

--- TrainManyModelsRF_Lab.R
+++ TrainManyModelsRF_Lab.R
@@ -5,26 +5,31 @@ library(caret)
 library(yardstick)   # model metrics (R2, RMSE)
 
 #Read in data
-speclib_rf<-read.csv("./R_outputs/speclib_dendrometers/dendrometer_canopy_bytreeid_5nm.csv")
-canopy_VIs<-read.csv("./R_outputs/speclib_dendrometers/veg_indices/dendrometer_VIs_5nm_bytreeid.csv")
+speclib_rf<-read.csv("./R_outputs/speclib_dendrometers/dendrometer_canopy_bytreeid.csv") #Note 1nm, 5nm, 10nm here
+canopy_VIs<-read.csv("./R_outputs/speclib_dendrometers/veg_indices/dendrometer_VIs_1nm_bytreeid.csv") #Note 1nm, 5nm, 10nm here
 drone_dendro<-read.csv("G:/Dendrometers/drone_dendro.csv")
 cumulative_zg<-read.csv("G:/Dendrometers/cumulative_zg.csv")
 
 #Make a list of  band names for data filtering later
-band_names<-colnames(speclib_rf[,30:ncol(speclib_rf)])
-VI_names<-colnames(canopy_VIs[,6:ncol(canopy_VIs)])
+#band_names<-colnames(speclib_rf[,30:ncol(speclib_rf)])
+#VI_names<-colnames(canopy_VIs[,6:ncol(canopy_VIs)])
 
 ##Decide which RF dataframe to use, set dataframe name to match workflow beneath
-#Joined spectral library & canopy VIs
-df_rf<-speclib_rf %>% 
-  inner_join( canopy_VIs, by = "TreeID", keep=FALSE)
+#Joined TWD, spectral library & canopy VIs
+df_rf <- speclib_rf %>% 
+  inner_join(canopy_VIs, by = "TreeID", keep = FALSE) %>%  
+  inner_join(drone_dendro %>% select(Dndrmtr, TWD), by = "Dndrmtr") %>%  
+  drop_na(TWD) %>%
+  select(-matches("X.x|CC.x|Species.x|X.1|sample_name|Site|Species.y|X.y|Y|CC.y|Dndrmtr|DBH|Tag"))
+
 #Joined TWD & canopy VIs
 df_rf <- canopy_VIs %>%
   inner_join(drone_dendro %>% select(Dndrmtr, TWD), by = "Dndrmtr") %>%
   drop_na(TWD)  # remove rows with no TWD value
 
 # Identify predictor columns
-predictor_vars <- names(canopy_VIs)[12:108]
+predictor_vars <- names(canopy_VIs)[12:108] #For just vegetation indicies & TWD
+predictor_vars <- names(df_rf)[2:640] #For vegetation indicies, spectral library, and TWD, adjust as needed
 print(predictor_vars)
 # Define response variable
 response_var <- "TWD"
@@ -35,7 +40,7 @@ model_data <- df_rf %>%
 
 #Split into training/testing sets
 set.seed(1234) # For reproducibility
-trainIndex <- createDataPartition(model_data[[response_var]], p = 0.8, list = FALSE)
+trainIndex <- createDataPartition(model_data[[response_var]], p = 0.7, list = FALSE)
 train_data <- model_data[trainIndex, ]
 test_data  <- model_data[-trainIndex, ]
 
@@ -43,7 +48,7 @@ test_data  <- model_data[-trainIndex, ]
 rf_model <- ranger(
   formula = TWD ~ .,  # formula shortcut for all predictors
   data = train_data,
-  num.trees = 500,    # 500 is reasonable?
+  num.trees = 1000,    # 500 is reasonable?
   importance = 'impurity'  # variable importance
 )
 print(rf_model)
